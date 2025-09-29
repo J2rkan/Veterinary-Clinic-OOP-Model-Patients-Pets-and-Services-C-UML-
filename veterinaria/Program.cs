fbc27cb50bf2c7915a505639d33ae7f2b9aa1c3a
jerkan <jerkan22@hotmail.com>
2025-09-29 17:45:22 -0500

    Rename classes and files to English, implement Patient-Pet association

--- veterinaria/Program.cs
+++ veterinaria/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using veterinaria.models;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Create Patient
+		Patient patient = new Patient("John Doe", 35, "123 Main St", "555-1234");
+
+		// Create Pets
+		Pet pet1 = new Pet("Buddy", "Dog", "Labrador", 5);
+		Pet pet2 = new Pet("Whiskers", "Cat", "Siamese", 3);
+
+		// Associate pets with patient
+		patient.AddPet(pet1);
+		patient.AddPet(pet2);
+
+		// Show patient info
+		patient.ShowInfo();
+
+		// Show all pets of the patient
+		patient.ShowAllPets();
+	}
+}
+
